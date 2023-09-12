81c7fc8a3bada71ac487e5983f2cb48f3d5b39df
husen <husenrasulov777@gmail.com>
2023-09-12 16:39:33 +0500

    some bugs are fixed

--- src/main/java/com/example/backend/Controller/MessageController.java
+++ src/main/java/com/example/backend/Controller/MessageController.java
@@ -2,6 +2,7 @@ package com.example.backend.Controller;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class MessageController {
 
     @PostMapping
     public HttpEntity<?> sendMessage(){
-        
+
         return null;
     }
 }
